119638e5884c7bd5ab0768b029194bf4d7dd325b
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-01-21 12:24:04 +0300

    First Trial

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,50 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries_dict = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(
+        bakeries_dict,
+        200
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = (Bakery.query.filter_by(id = id).first()).to_dict()
+
+    response = make_response(
+        bakery,
+        200
+    )
+
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bakedgoods = [bakedgood.to_dict() for bakedgood in BakedGood.query.order_by(desc(BakedGood.price)).all()]
+
+    response = make_response(
+        bakedgoods,
+        200
+    )
+
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+     
+    bakedgoods = [bakedgood.to_dict() for bakedgood in BakedGood.query.order_by(desc(BakedGood.price)).all()][0]
+
+    response = make_response(
+        bakedgoods,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
